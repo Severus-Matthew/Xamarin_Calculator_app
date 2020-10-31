e1a61a06aff601f56514e843f3d2d6939ada2fe1
Severus-Matthew <drprofmjha@gmail.com>
2020-10-31 20:44:35 +0530

    added files

---
+++ Calc/Calc/obj/Debug/90/android/src/crc6488401d563ae911af/MainActivity.java
@@ -0,0 +1,56 @@
+package crc6488401d563ae911af;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_ButtonClick:(Landroid/view/View;)V:__export__\n" +
+			"";
+		mono.android.Runtime.register ("Calc.MainActivity, Calc", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Calc.MainActivity, Calc", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void ButtonClick (android.view.View p0)
+	{
+		n_ButtonClick (p0);
+	}
+
+	private native void n_ButtonClick (android.view.View p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
